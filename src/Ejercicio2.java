d9c06c25ae2d9cfdc6cd78a943d7676b7f9ba379
B-404notfound <eliaslaramartinez@gmail.com>
2022-08-22 23:29:56 -0400

    carga inicial

---
+++ src/Ejercicio2.java
@@ -0,0 +1,13 @@
+public class Ejercicio2 {
+
+    public static void main (String []args){
+        int contador = 0;
+
+        while (contador <3){
+            System.out.println(contador);
+            contador = contador+1 ;
+        }
+
+
+    }
+}
---
+++ src/Ejercicio3.java
@@ -0,0 +1,11 @@
+public class Ejercicio3 {
+
+    public static void main (String [] args){
+        int contador = 0;
+
+        do {
+            System.out.println(contador);
+            contador = contador +1;
+        }while (contador<3);
+    }
+}
